88bd6f89842226f3518345b09b57e768eeaa24dd
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-05-05 21:02:59 +0100

    foreign commits on my branch

---
+++ _fakeeditor.sh
@@ -0,0 +1,2 @@
+sed -e "s/A message/An edited message/g" <"$1" >"$1-"
+
---
+++ foreign_commits_on_my_branch.sh
@@ -0,0 +1,37 @@
+# !/bin/sh
+
+set -e
+source "./_init_repo.sh"
+
+REPO_NAME="$(basename $0 '.sh')"
+
+init_repo $REPO_NAME
+cd workspace/$REPO_NAME
+
+git checkout -b feat/2
+
+echo "content" > file1.txt
+git add .
+git commit -m "first commit (other branch)"
+
+echo "content2" > file2.txt
+git add .
+git commit -m "second commit (other branch)"
+
+git checkout main
+git checkout -b feat/1
+
+echo "content3" > file3.txt
+git add .
+git commit -m "first commit"
+
+git merge feat/2 -m "Merge branch feeat/2 into feat/1"
+
+echo 'sed -e "s/commit/commit (edited)/g" <"$1" >"$1-"' > fake_editor.sh
+echo 'mv "$1-" "$1"' >> fake_editor.sh
+chmod +x fake_editor.sh
+
+GIT_EDITOR="./fake_editor.sh" \
+  GIT_SEQUENCE_EDITOR="sed -i '1s/^pick/r/g'" \
+  git rebase -i HEAD~2 
+

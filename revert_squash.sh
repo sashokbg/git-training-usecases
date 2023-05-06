bfbdab8106ca083218f8be966f7bad898304affc
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2023-05-06 09:41:03 +0100

    optimized revert squash use cases

--- revert_squash.sh
+++ revert_squash.sh
@@ -20,6 +20,12 @@ touch file3.txt
 git add file3.txt
 git commit -m "third"
 
+echo 'echo "bad merged commit" >"$1-"' > fake_editor.sh
+echo 'mv "$1-" "$1"' >> fake_editor.sh
+chmod +x fake_editor.sh
+
 GIT_SEQUENCE_EDITOR="sed -i '2s/^pick/squash/g'" \
+  GIT_EDITOR="./fake_editor.sh" \
   git rebase -i HEAD~2 
+
 
--- revert_squash_pushed.sh
+++ revert_squash_pushed.sh
@@ -1,27 +1,7 @@
 # !/bin/sh
 
 set -e
-source "./_init_repo.sh"
-
-REPO_NAME="$(basename $0 '.sh')"
-
-init_repo $REPO_NAME
-cd workspace/$REPO_NAME
-
-touch file1.txt
-git add file1.txt
-git commit -m "first"
-
-touch file2.txt
-git add file2.txt
-git commit -m "second"
-
-touch file3.txt
-git add file3.txt
-git commit -m "third"
-
-GIT_SEQUENCE_EDITOR="sed -i '2s/^pick/squash/g'" \
-  git rebase -i HEAD~2 
+source "./revert_squash.sh"
 
 git push origin main --force
 

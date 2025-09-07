bec4a5e7ba57afe2dc4b0954ee803075b39d10d7
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2025-09-07 18:50:24 +0200

    feat(exercise): committed sensitive date

--- scripts/accidentally_commited_password.sh
+++ scripts/accidentally_commited_password.sh
@@ -1,14 +1,5 @@
 . "./_init_repo.sh"
 
-echo "*********************"
-echo "I accidentally committed and pushed a password in file called password.txt"
-echo "Now I need to make sure it is not recoverable."
-echo ""
-echo "Press a key to continue."
-echo "*********************"
-
-read read_var
-
 REPO_NAME="$(basename $0 '.sh')"
 
 init_repo $REPO_NAME

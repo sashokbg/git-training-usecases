5821fa9741b8894d13ca8a66107ab527fd5ded2f
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2025-09-19 13:54:00 +0200

    feat: added get most recent code from main exercise

--- scripts/get_most_recent_code_from_main.sh
+++ scripts/get_most_recent_code_from_main.sh
@@ -8,19 +8,25 @@ REPO_NAME="get_most_recent_code_from_main"
 init_repo $REPO_NAME
 cd workspace/$REPO_NAME
 
+git branch feat
+
 echo "content" > file1.txt
+git add .
+git commit -m "first commit (main)"
+
+git checkout feat
+
+echo "content_modified" > file1.txt
 git add .
 git commit -m "first commit"
 
-git branch feat
-
 echo "content2" > file2.txt
 git add .
-git commit -m "second commit"
+git commit -m "third commit"
 
 echo "content3" > file3.txt
 git add .
-git commit -m "third commit"
+git commit -m "fourth commit"
 
 git push origin main
 git push origin feat
